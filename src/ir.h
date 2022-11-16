80484339bd5b8c8146fa78dcb759cfb9495f24d3
zawie <zawie@rice.edu>
2022-11-16 16:29:19 -0600

    define ir

---
+++ src/ir.h
@@ -0,0 +1,21 @@
+typedef unsigned int ir_t;
+
+enum op_t {
+    rshift  = 0,    // >
+    lshift  = 1,    // <
+    inc     = 2,    // +
+    dec     = 3,    // -
+    input   = 4,    // ,
+    output  = 5,    // .
+    bloop   = 6,    // [
+    eloop   = 7     // ]
+}
+
+#define GET_OP (ir)     (ir &   0b111)
+#define SET_OP (ir, op) (ir & (~0b111 ^ op))
+
+#define GET_COUNT (ir)        (ir >> 3)
+#define SET_COUNT (ir, count) (ir & (0b111 ^ (count << 3)))
+
+
+
